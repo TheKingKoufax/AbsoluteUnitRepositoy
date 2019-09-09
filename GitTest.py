9e1fb457b987b14ceb6a9f2c43210c1bc10eea33
Koufax Cranmer <kcranmer@davidsonacademy.unr.edu>
2019-09-09 12:27:17 -0700

    yw

--- GitTest.py
+++ GitTest.py
@@ -1,4 +1,4 @@
 print("Hello World")
 print("Hello Again")
 print("So you jus gon bring a birthhday gift...")
-nSamples= int(input("Guess a number between 1 and 10 (m):")
+int(input("Guess a number between 1 and 10 (m):"))
